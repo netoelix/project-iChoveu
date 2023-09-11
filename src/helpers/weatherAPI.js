d968fb18fddbcb98233d0c49fc162bf7cc351e2c
Elix Neto <neto.elix@outlook.com>
2023-09-11 20:30:00 -0300

    Finalizando requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,24 @@
-// Remova os comentários a medida que for implementando as funções
-
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const token = import.meta.env.VITE_TOKEN;
+// `http://api.weatherapi.com/v1/current.json?key=${token}&q=${term}`
+export const searchCities = async (term) => {
+  const city = await fetch(`http://api.weatherapi.com/v1/search.json?key=${token}&q=${term}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length === 0) {
+        return window.alert('Nenhuma cidade encontrada');
+      }
+      return data;
+    });
+  return city;
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+export const getWeatherByCity = async (cityURL) => {
+  const city = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${token}&q=${cityURL}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return { temp: data.current.temp_c,
+        condition: data.current.condition.text,
+        icon: data.current.condition.icon };
+    });
+  return city;
 };
