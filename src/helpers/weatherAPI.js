520ab9ee6008a40a77448608750f28cbde1359a3
Elix Neto <neto.elix@outlook.com>
2023-09-12 20:31:21 -0300

    Corrigindo requisito 1, tirando o return do alert

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -5,35 +5,43 @@ export async function searchCities(term) {
     .then((response) => response.json())
     .then((data) => {
       if (data.length === 0) {
-        return window.alert('Nenhuma cidade encontrada');
+        window.alert('Nenhuma cidade encontrada');
       }
       return data;
-    })
-    .catch(console.log('Erro eo acessar a API na função searchCities'));
+    });
   return city;
+  // const city = await fetch(`http://api.weatherapi.com/v1/search.json?key=${token}&q=${term}`);
+  // const jsonReturn = city.json();
+
+  // if (jsonReturn.length === 0) {
+  //   return window.alert('Nenhuma cidade encontrada');
+  // }
+  // return jsonReturn;
 }
 
 export async function getWeatherByCity(cityURL) {
-  // const city = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${token}&q=${cityURL}`)
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     return { temp: data.current.temp_c,
-  //       condition: data.current.condition.text,
-  //       icon: data.current.condition.icon };
-  //   })
-  //   .catch(console.log('Erro eo acessar a API na função getWeatherByCity'));
-  // return city;
+  const city = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${token}&q=${cityURL}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return { name: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.text,
+        icon: data.current.condition.icon,
+        url: cityURL };
+    });
+  return city;
 
-  const apiFetch = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${token}&q=${cityURL}`);
-  const jsonReturn = await apiFetch.json();
-  const { location, current } = jsonReturn;
-  const newReturn = {
-    name: location.name,
-    country: location.country,
-    temp: current.temp_c,
-    condition: current.condition.text,
-    icon: current.condition.icon,
-    url: cityURL,
-  };
-  return newReturn;
+  // const apiFetch = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${token}&q=${cityURL}`);
+  // const jsonReturn = await apiFetch.json();
+  // const { location, current } = jsonReturn;
+  // const newReturn = {
+  //   name: data.location.name,
+  //   country: data.location.country,
+  //   temp: data.current.temp_c,
+  //   condition: data.current.condition.text,
+  //   icon: data.current.condition.icon,
+  //   url: cityURL,
+  // };
+  // return newReturn;
 }
